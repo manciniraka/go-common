a93de30b74a50a33c302171f7f5e113384fc242e
manciniraka <manciniraka@gmail.com>
2026-06-12 15:00:02 +0700

    feat(database): add postgres connection package

---
+++ database/postgres.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectPostgres() (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s post=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSLMODE"),
+	)
+
+	db, err := gorm.Open(
+		postgres.Open(dsn),
+		&gorm.Config{},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
---
+++ database/postgres_test.go
@@ -0,0 +1,7 @@
+package database
+
+import "testing"
+
+func TestPackageExists(t *testing.T) {
+	_ = ConnectPostgres
+}
