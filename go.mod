c885706fc8f3de471336222c2bc73ff3b1eb2154
manciniraka <manciniraka@gmail.com>
2026-06-12 14:24:34 +0700

    feat(validator): add echo validator package

--- go.mod
+++ go.mod
@@ -2,4 +2,16 @@ module github.com/manciniraka/go-common
 
 go 1.25.0
 
-require golang.org/x/crypto v0.53.0
+require (
+	github.com/go-playground/validator/v10 v10.30.3
+	golang.org/x/crypto v0.53.0
+)
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.13 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	golang.org/x/sys v0.46.0 // indirect
+	golang.org/x/text v0.38.0 // indirect
+)
---
+++ validator/validator_test.go
@@ -0,0 +1,36 @@
+package validator
+
+import "testing"
+
+type UserRequest struct {
+	Name  string `validate:"required"`
+	Email string `validate:"required,email"`
+}
+
+func TestValidatorSuccess(t *testing.T) {
+	validate := New()
+
+	payload := UserRequest{
+		Name:  "Raka",
+		Email: "raka@mail.com",
+	}
+
+	err := validate.Validate(payload)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestValidatorFailed(t *testing.T) {
+	validate := New()
+
+	payload := UserRequest{
+		Name:  "",
+		Email: "invalid-email",
+	}
+
+	err := validate.Validate(payload)
+	if err != nil {
+		t.Fatal("expected validation error")
+	}
+}
