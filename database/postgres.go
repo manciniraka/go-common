ee8083150948223041168f218f51633739a98c0c
manciniraka <manciniraka@gmail.com>
2026-06-14 08:30:13 +0700

    fix(database): correct postgres dsn port configuration

--- database/postgres.go
+++ database/postgres.go
@@ -11,7 +11,7 @@ import (
 
 func ConnectPostgres() (*gorm.DB, error) {
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s post=%s sslmode=%s",
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
